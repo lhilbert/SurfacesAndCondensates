d701f8865308fc3f21262a80d4141366a17f1777
Lennart Hilbert <lennart.hilbert@gmail.com>
2024-09-19 15:51:27 +0200

    Added script for example images, added scatter plots to overview script

--- Titration_ExampleImages.m
+++ Titration_ExampleImages.m
@@ -3,23 +3,36 @@ clear all
 %% Plot the example images
 
 SourceFileCell = {...
-	'./ExtractedStacks_Stages/Oblong/Cond_2/Image_8.mat',...
-	};
+	'./ExtractedStacks/Cond_1/Image_10.mat',...
+	'./ExtractedStacks/Cond_2/Image_3.mat',...
+    './ExtractedStacks/Cond_3/Image_4.mat',...
+	'./ExtractedStacks/Cond_4/Image_2.mat',...
+	'./ExtractedStacks/Cond_5/Image_5.mat',...
+	'./ExtractedStacks/Cond_6/Image_6.mat',...
+    };
 
-OP_blurRange = 0.03; % micrometers
-Ser2P_blurRange = 0.01; % micrometers
-Ser5P_blurRange = 0.01; % micrometers
+Surf_blurRange = 0.1; % micrometers
+Cond_blurRange = 0.1; % micrometers
 
+imgSources = [1,2,3,4,5,6];
 imgRanges = {...
 	[37,37,72,72]+[-6,+6,-6,+6]};
-% imgRanges = {...
-% 	[0,60,0,80]};
-zCoordinate = [34]; % 17,33
-rotate_flag = [false];
+imgRanges = {...
+	[75,75,110,110]+[-20,+20,-15,+15],...
+    [75,75,110,110]+[-20,+20,-15,+15],...
+    [75,75,115,115]+[-20,+20,-15,+15],...
+    [60,60,142,142]+[-20,+20,-15,+15],...
+    [100,100,140,140]+[-20,+20,-15,+15],...
+    [75,75,115,115]+[-20,+20,-15,+15],...
+    };
+zCoordinate = [35,30,26,5,35,10];
+rotate_flag = [false,false,false,false,false,false];
 
-plotTitles = {'klf2b'};
+plotTitles = {'+Surf','+Surf','+Surf','+Surf','+Surf','-Surf'};
 
-scaleBar = 4.0; % in microns
+scaleBar = 10.0; % in microns
+
+% --- parameters end
 
 numPlots = numel(imgRanges);
 
@@ -32,55 +45,46 @@ for pp = 1:numPlots
 	
 	thisFilePath = SourceFileCell{pp};
 	loadStruct = load(thisFilePath,...
-		'imgStack','imgSize','pixelSize','zStepSize');
+		'imgStack','imgSize','pixelSize','zStepSize','condName');
 	imgStack = loadStruct.imgStack;
 	imgSize = loadStruct.imgSize;
 	pixelSize = loadStruct.pixelSize;
 	zStepSize = loadStruct.zStepSize;
+    condName = loadStruct.condName;
 
 	thisImg = imgStack;
 	thisPixelSize = pixelSize;
 	
 	% --- Removal of DNA background
-	OP_img = double(thisImg{3}(:,:,zCoordinate(pp)));
-	Ser2P_img = double(thisImg{1}(:,:,zCoordinate(pp)));
-	Ser5P_img = double(thisImg{2}(:,:,zCoordinate(pp)));
-	
+	Surf_img = double(thisImg{1}(:,:,zCoordinate(pp)));
+	Cond_img = (double(thisImg{2}(:,:,zCoordinate(pp))));
+
 	if rotate_flag(pp)
-		OP_img = OP_img';
-		Ser2P_img = Ser2P_img';
-		Ser5P_img = Ser5P_img';
+		Surf_img = Surf_img';
+		Cond_img = Cond_img';
 	end
 
-	OP_img = imgaussfilt(OP_img,...
-		OP_blurRange./thisPixelSize);
-	Ser5P_img = imgaussfilt(Ser5P_img,...
-		Ser5P_blurRange./thisPixelSize);
-	Ser2P_img = imgaussfilt(Ser2P_img,...
-		Ser2P_blurRange./thisPixelSize);
+	Surf_img = imgaussfilt(Surf_img,...
+		Surf_blurRange./thisPixelSize);
+	Cond_img = imgaussfilt(Cond_img,...
+		Cond_blurRange./thisPixelSize);
 	
 	thisImgRange = round(imgRanges{pp}./thisPixelSize)+1;
-	OP_img = OP_img(...
+	Surf_img = Surf_img(...
 		thisImgRange(1):thisImgRange(2),...
 		thisImgRange(3):thisImgRange(4));
-	Ser2P_img = Ser2P_img(...
+	Cond_img = Cond_img(...
 		thisImgRange(1):thisImgRange(2),...
 		thisImgRange(3):thisImgRange(4));
-	Ser5P_img = Ser5P_img(...
-		thisImgRange(1):thisImgRange(2),...
-		thisImgRange(3):thisImgRange(4));
-	thisSize = size(Ser5P_img);
+	thisSize = size(Cond_img);
 	
-	OP_lims = prctile(OP_img(:),[0.25,99.95]);
-	Ser5P_img = Ser5P_img./median(Ser5P_img(:));
-	Ser5P_lims = [0.5,3.5];
-	Ser2P_lims = prctile(Ser2P_img(:),[5,99.9]);
-	
-	
-	subplot(3,numPlots.*2,numPlots.*0+1+(pp-1).*2)
+    Surf_lims = prctile(Surf_img(:),[0.01,99.99]);
+    Cond_lims = prctile(Cond_img(:),[0.01,99.99]);
+
+	subplot(3,numPlots,numPlots.*0+pp)
 	imagesc([0,thisSize(2)].*thisPixelSize,...
 		[0,thisSize(1)].*thisPixelSize,...
-		OP_img,OP_lims)
+		Surf_img,Surf_lims)
 	axis equal tight
 	set(gca,'XTick',[],'YTick',[])
 	set(gca,'YDir','normal')
@@ -88,44 +92,37 @@ for pp = 1:numPlots
 	plot([1,1+scaleBar],thisSize(1).*thisPixelSize-[1,1],...
 		'w-','LineWidth',3) % Plot scale bar
 	textObject = ...
-		text(1+0.5.*scaleBar,thisSize(2).*thisPixelSize-2.0,...
+		text(1+0.5.*scaleBar,thisSize(1).*thisPixelSize-2.0,...
 		sprintf('%d \\mum',scaleBar));
 	set(textObject,'Color',[1,1,1],'HorizontalAlignment','center',...
 		'FontSize',12)
-	ylabel('Oligopaint')
+	ylabel('Surface')
+    title(condName)
+    xlabel(sprintf('Int. range [%d,%d]',...
+        round(Surf_lims(1)),round(Surf_lims(2))))
 	
 	
-	subplot(3,numPlots.*2,numPlots.*2+1+(pp-1).*2)
+	subplot(3,numPlots,numPlots.*1+pp)
 	imagesc([0,thisSize(2)].*thisPixelSize,...
 		[0,thisSize(1)].*thisPixelSize,...
-		Ser5P_img,Ser5P_lims)
+		Cond_img,Cond_lims)
 	axis equal tight
 	set(gca,'XTick',[],'YTick',[])
 	set(gca,'YDir','normal')
-	ylabel('Pol II S5P')
+	ylabel('X-Motif')
+    xlabel(sprintf('Int. range [%d,%d]',...
+        round(Cond_lims(1)),round(Cond_lims(2))))
+	
+	% --- Color merge
 
-	
-	subplot(3,numPlots.*2,numPlots.*4+1+(pp-1).*2)
-	imagesc([0,thisSize(2)].*thisPixelSize,...
-		[0,thisSize(1)].*thisPixelSize,...
-		Ser2P_img,Ser2P_lims)
-	axis equal tight
-	set(gca,'XTick',[],'YTick',[])
-	set(gca,'YDir','normal')
-	ylabel('Pol II S2P')
-	colormap(inferno)
-	
-	
-	
-	% --- OP / Ser5P
+    subplot(3,numPlots,numPlots.*2+pp)
+
 	% Prepare the color channels for RGB plotting
 	this_magenta_plot = ...
-		(Ser5P_img-Ser5P_lims(1))./diff(Ser5P_lims);
+		(Cond_img-Cond_lims(1))./diff(Cond_lims);
 	this_green_plot = ...
-		(OP_img-OP_lims(1))./diff(OP_lims);
-	thisSize = size(OP_img);
-	
-	subplot(3,numPlots.*2,numPlots.*0+2+(pp-1).*2)
+		(Surf_img-Surf_lims(1))./diff(Surf_lims);
+	thisSize = size(Surf_img);
 	
 	redChannel = this_magenta_plot;
 	blueChannel = this_magenta_plot;
@@ -140,59 +137,7 @@ for pp = 1:numPlots
 		rgb_img)
 	set(gca,'XTick',[],'YTick',[])
 	set(gca,'YDir','normal')
-	ylabel('OP / Pol II Ser5P')
-	axis equal tight
-	
-	% --- OP / Ser2P
-	% Prepare the color channels for RGB plotting
-	this_magenta_plot = ...
-		(Ser2P_img-Ser2P_lims(1))./diff(Ser2P_lims);
-	this_green_plot = ...
-		(OP_img-OP_lims(1))./diff(OP_lims);
-	thisSize = size(OP_img);
-	
-	subplot(3,numPlots.*2,numPlots.*2+2+(pp-1).*2)
-	
-	redChannel = this_magenta_plot;
-	blueChannel = this_magenta_plot;
-	greenChannel = this_green_plot;
-	rgb_img = zeros(thisSize(1),thisSize(2),3);
-	rgb_img(:,:,1) = redChannel;
-	rgb_img(:,:,2) = greenChannel;
-	rgb_img(:,:,3) = blueChannel;
-	
-	image([0,thisSize(2)].*thisPixelSize,...
-		[0,thisSize(1)].*thisPixelSize,...
-		rgb_img)
-	set(gca,'XTick',[],'YTick',[])
-	set(gca,'YDir','normal')
-	ylabel('OP / Pol II Ser2P')
-	axis equal tight
-	
-	% --- OP / Ser2P
-	% Prepare the color channels for RGB plotting
-	this_magenta_plot = ...
-		(Ser5P_img-Ser5P_lims(1))./diff(Ser5P_lims);
-	this_green_plot = ...
-		(Ser2P_img-Ser2P_lims(1))./diff(Ser2P_lims);
-	thisSize = size(OP_img);
-	
-	subplot(3,numPlots.*2,numPlots.*4+2+(pp-1).*2)
-	
-	redChannel = this_magenta_plot;
-	blueChannel = this_magenta_plot;
-	greenChannel = this_green_plot;
-	rgb_img = zeros(thisSize(1),thisSize(2),3);
-	rgb_img(:,:,1) = redChannel;
-	rgb_img(:,:,2) = greenChannel;
-	rgb_img(:,:,3) = blueChannel;
-	
-	image([0,thisSize(2)].*thisPixelSize,...
-		[0,thisSize(1)].*thisPixelSize,...
-		rgb_img)
-	set(gca,'XTick',[],'YTick',[])
-	set(gca,'YDir','normal')
-	ylabel('Pol iI Ser5P / Pol II Ser2P')
+	ylabel('Surface / X-Motif')
 	axis equal tight
 	
 end
